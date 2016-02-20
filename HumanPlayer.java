5eb6685ea18e577c62c7f1e48feebd31a0409ab0
janicelihuang <janicelihuang@gmail.com>
2016-02-20 17:58:36 -0600

    Added HumanPlayer

---
+++ HumanPlayer.java
@@ -0,0 +1,19 @@
+public class HumanPlayer implements Player{
+	String name;
+
+	public HumanPlayer (String name){
+		this.name = name;
+	}
+
+	public int MakeMove(){
+	}
+	
+	public String getName(){
+	}
+
+	public void updateData(int [][]board){
+	}
+
+	public void won(int i){
+	}
+}
