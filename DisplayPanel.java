c13530ed73548ac15695de3f62ed59ff8b02c93a
janicelihuang <janicelihuang@gmail.com>
2016-02-20 15:36:46 -0600

    finally fixed scaling

--- DisplayPanel.java
+++ DisplayPanel.java
@@ -7,22 +7,23 @@ public class DisplayPanel extends JPanel{
 
 	public DisplayPanel(){
 		setBackground(Color.white);
-		pieces = new int [6][7];
+		pieces = new int [7][6];
 	}
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		int pieceSize = this.getHeight()/6;
-		int backgroundWidth = this.getWidth()*1200/1600;
+		
+		double scale = Math.min(this.getHeight()/1200., this.getWidth()/1600.);
+
 		for (int i = 0; i < 6; i++)
 			for (int j = 0; j < 7; j++)
 			{
 				if(pieces[i][j] == 1)
-					g.drawImage((new ImageIcon("Blue_Piece.png")).getImage(), backgroundWidth*j/7,(this.getHeight()+10)*i/6, pieceSize, pieceSize,null);
+					g.drawImage((new ImageIcon("Blue_Piece.png")).getImage(), (int)((52+j*225)*scale),(int)((27+i*200)*scale), (int)(150*scale), (int)(150*scale),null);
 				else if (pieces[i][j] == 2)
-					g.drawImage((new ImageIcon("Red_Piece.png")).getImage(), backgroundWidth*j/7,(this.getHeight()+10)*i/6, pieceSize, pieceSize,null);
+					g.drawImage((new ImageIcon("Red_Piece.png")).getImage(), (int)((52+j*225)*scale),(int)((27+i*200)*scale), (int)(150*scale), (int)(150*scale),null);
 			}
-		g.drawImage((new ImageIcon("Board.png")).getImage(), 0 ,0, backgroundWidth, this.getHeight(), null);
+		g.drawImage((new ImageIcon("Board.png")).getImage(), 0 ,0, (int)(1600*scale), (int)(1200*scale), null);
 	}
 	
 	public void update(int [][] pieces){
