6dc18933110c89a45449296ac8ede17eca2c5842
Springo <kevinmxia@gmail.com>
2016-02-20 04:44:13 -0500

    changed file IO style

--- AIPlayer.java
+++ AIPlayer.java
@@ -2,38 +2,37 @@
 //Kevin Xia, Janice Huang, Nikhil Dave
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 public class AIPlayer implements Player {
 	private String name;
-	private HashMap<String, String> brain;
+	private Map<String, Integer> brain;
 	private ArrayList<String> experience;
 	
 	public AIPlayer(String name) {
 		this.name = name;
-		brain = new HashMap<String, String>();
+		brain = new HashMap<String, Integer>();
 		experience = new ArrayList<String>();
 	}
 	
 	//Following methods only for testing.
 	public static void main(String [] args) {
 		AIPlayer p = new AIPlayer("Bob");
-		p.addnew("0 0 0 0 0 1");
-		p.addnew("0 0 0 1 0 0");
-		p.addnew("0 0 1 0 1 1");
+		p.addnew("0 0 0 0 0 ", 10);
+		p.addnew("0 0 0 1 0 ", 9);
+		p.addnew("0 0 1 0 1 ", 8);
 		p.writeBrain(p.getName() + "Brain.txt");
 		p.getBrain(p.getName() + "Brain.txt");
-		System.out.println(p.getMap("0 0 0 0 "));
-		System.out.println(p.getMap("0 0 0 1 "));
-		System.out.println(p.getMap("0 0 1 0 "));
+		System.out.println(p.getMap("0 0 0 0 0 "));
+		System.out.println(p.getMap("0 0 0 1 0 "));
+		System.out.println(p.getMap("0 0 1 0 1 "));
 	}
 	
-	public void addnew(String input) {
-		experience.add(input);
+	public void addnew(String key, int input) {
+		brain.put(key, input);
 	}
 	
-	public String getMap(String key) {
+	public Integer getMap(String key) {
 		return brain.get(key);
 	}
 	//End test methods.
@@ -53,7 +52,7 @@ public class AIPlayer implements Player {
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String node;
 			while ((node = br.readLine()) != null) {
-				brain.put(node.substring(0, node.length() - 3), node.substring(node.length() - 3));
+				brain.put(node.substring(0, node.length() - 6), Integer.parseInt(node.substring(node.length() - 6)));
 			}
 			br.close();
 		}
@@ -69,8 +68,15 @@ public class AIPlayer implements Player {
 		try {
 			f.createNewFile();
 			PrintWriter pw = new PrintWriter(new FileOutputStream(f));
-			for (int i = 0; i < experience.size(); i++) {
-				pw.println(experience.get(i));
+			Iterator it = brain.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry pair = (Map.Entry)it.next();
+				int value = (Integer)(pair.getValue());
+				String zeroes = "";
+				for (int i = 0; i < (6 - (Integer.toString(value)).length()); i++)
+					zeroes += "0";
+				pw.println((String)(pair.getKey()) + zeroes + (Integer)(pair.getValue()));
+				it.remove();
 			}
 			pw.close();
 		}
