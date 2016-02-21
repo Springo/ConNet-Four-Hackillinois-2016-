66744257dad19ba5a2008e475df57e3deb7719c1
Springo <kevinmxia@gmail.com>
2016-02-20 19:19:55 -0500

    added functionality to HumanPlayer

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,14 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class HumanPlayer implements Player{
 	String name;
 
 	public HumanPlayer (String name){
 		this.name = name;
 	}
+	
+	public static void main (String[] args) {
+		HumanPlayer hp = new HumanPlayer("Bob");
+		System.out.println(hp.makeMove());
+	}
 
-	public int MakeMove(){
+	public int makeMove(){
+		Object[] possibleValues = { "1", "2", "3", "4", "5", "6", "7" };
+		Object selectedValue = JOptionPane.showInputDialog(null, "Choose a column: ", name + "'s Move", JOptionPane.INFORMATION_MESSAGE, null, possibleValues, possibleValues[0]);
+		return Integer.parseInt((String)selectedValue) - 1;
 	}
 	
 	public String getName(){
+		return name;
 	}
 
 	public void updateData(int [][]board){
